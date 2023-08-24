b88984f080527a016fef650e5e58d55fe0878934
Your Name <you@example.com>
2023-08-24 08:34:02 +0200

    druhý commit

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>Příklad aplikace PHP v akci...</b></p>
+		<p><b>Pozor, Jarda toto změnil.....</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>TIME FOR A COLD BEER</h2>
+	
 	</body> 
 </html>
